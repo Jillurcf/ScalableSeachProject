8e9ad429d4c00ea0a47ab0e92be01ba042551872
Jillur Rahman <112783203+Jillurcf@users.noreply.github.com>
2025-09-26 15:21:44 +0600

    Update elasticsearchClient.js

--- elasticsearchClient.js
+++ elasticsearchClient.js
@@ -1,21 +1,14 @@
-// const { Client } = require('@elastic/elasticsearch');
 
-// const client = new Client({
-//   node: 'https://localhost:9200', // Update if using remote Elastic server
-// });
-
-// module.exports = client;
-// elasticsearch.js
 const { Client } = require('@elastic/elasticsearch');
 
 const client = new Client({
   node: 'https://localhost:9200',
   auth: {
     username: 'elastic',
-    password: '=S57ym5rExp7eCsJIztW', // Replace this with the real password
+    password: '=S57ym5rExp7eCsJIztW', 
   },
   tls: {
-    rejectUnauthorized: false, // Accept self-signed cert in dev
+    rejectUnauthorized: false, 
   },
 });
 
